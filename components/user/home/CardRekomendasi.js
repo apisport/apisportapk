04f7572c47b28aa62fb07063c5779f675a57138e
apisport <api.sport.team@gmail.com>
2022-07-15 16:01:24 +0700

    div2

--- components/user/home/CardRekomendasi.js
+++ components/user/home/CardRekomendasi.js
@@ -12,7 +12,7 @@ const CardRekomendasi = ({ props }) => {
                 }
 
             }}>
-                <div className="col card shadow">
+                <div className="col-12 col-md-4 card shadow">
                     <div className="card text-start">
                         <div id={`${namaHasil}`} className="carousel slide" data-bs-ride="carousel">
                             <div className="carousel-indicators">
