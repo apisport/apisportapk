aa307271f3bd8f1737865d73ee7d7a7aad5dda62
apisport <api.sport.team@gmail.com>
2022-09-21 16:46:53 +0700

    pwa n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    scope: '/app',
+    // disable: process.env.NODE_ENV === 'development',
+    // register: true,
+    // scope: '/app',
     // sw: 'service-worker.js',
     //...
 })
